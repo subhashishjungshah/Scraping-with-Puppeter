191940151c391ee702730d6a21f9806bc803131c
subhashishshah <subhashish@ismt.edu.np>
2023-09-18 22:02:11 +0545

    added automation script

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello Subhashish");
+const puppeteer = require("puppeteer-core");
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/chromium-browser",
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://google.com");
+  await page.screenshot({ path: "example.png" });
+
+  await browser.close();
+})();
